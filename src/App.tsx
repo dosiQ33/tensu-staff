f4c78cd218b5aa36932b474468cc7ab3a1a858bd
Madi Abzhanov <abzhanma@gmail.com>
2025-05-19 18:19:58 +0500

    add mainpage2

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ import {
   Navigate,
 } from 'react-router-dom';
 import OnboardingPage from './pages/onboarding/Onboarding';
-import MainPage from './pages/main/MainPage';
+import MainPage2 from './pages/main/MainPage2';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/temp" element={<OnboardingPage />} />
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<MainPage2 />} />
         {/* catch-all */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
