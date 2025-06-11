9552cb4045e560c7a0974d2c35f06ece44be7622
Madi Abzhanov <abzhanma@gmail.com>
2025-06-11 13:49:38 +0500

    test

--- src/pages/student-pages/onboarding/Onboarding.tsx
+++ src/pages/student-pages/onboarding/Onboarding.tsx
@@ -77,7 +77,7 @@ export default function OnboardingPage() {
     if (!contactData?.response) return;
     const postAndNavigate = async () => {
       try {
-        const resp = await fetch("/api/v1/stuff", {
+        const resp = await fetch("https://training-tracker-mini-app.vercel.app/api/v1/stuff/", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
