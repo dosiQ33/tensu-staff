f9c1b84fc98395717500b5273e62c321791b4496
Madi Abzhanov <abzhanma@gmail.com>
2025-08-28 03:00:29 +0500

    add

--- src/pages/coach-pages/profile/components/CreateClubModal.tsx
+++ src/pages/coach-pages/profile/components/CreateClubModal.tsx
@@ -198,8 +198,7 @@ export const CreateClubModal: React.FC<CreateClubModalProps> = ({
         </div>
 
         {/* Actions */}
-        <div className="flex justify-end px-6 pt-4 pb-10 bg-gray-100 h-full">
-          <button
+        <div className="flex justify-end px-6 py-4 bg-gray-100 shrink-0">          <button
             onClick={handleSubmit}
             disabled={loading || !form.name}
             className="inline-flex items-center px-5 py-2 h-max bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300 transition disabled:opacity-50"
